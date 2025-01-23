c2b14ca4bbee538e4ac86550297cab44aec7be67
angelreyes117 <angelreyes.117@gmail.com>
2025-01-23 01:42:58 +0000

    header file

---
+++ shell.h
@@ -0,0 +1,18 @@
+/*
+ * File: simple_shell.h
+ * Author: Angel Reyes and Diego Esquivel
+ * Description: Header file for the simple UNIX command line interpreter.
+ */
+
+#ifndef SIMPLE_SHELL_H
+#define SIMPLE_SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+
+extern char **environ;
+
+#endif /* SIMPLE_SHELL_H */
